ffdca206cb9c5cd828fd5c226414b441e3721cb1
dev-alamin1 <dev.alaminmondal@gmail.com>
2022-10-26 19:13:33 +0600

    fake courses data added

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express')
+// cors middleware 
+const cors = require('cors')
 const app = express()
 const port = 5000
+const courses = require('./data/courses.json')
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Dev Skill Server running...')
+})
+
+app.get('/courses',(req,res)=>{
+    res.send(courses)
 })
 
 app.listen(port, () => {
